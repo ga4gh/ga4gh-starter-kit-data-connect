2146c283d85cf89baa0d53d5e483c9a5437cd858
Alex Tsai <alextsaihi@gmail.com>
2022-05-02 10:48:07 -0400

    Update to_json.py

--- models/to_json.py
+++ models/to_json.py
@@ -1,14 +1,11 @@
 import json
 
-def tsv_to_json(tsv_file,json_file):
+def tsv_to_json(tsv_file):
 	arr = []
 	file = open(tsv_file, 'r')
 	a = file.readline()
 	wgs_cram_drs_uri = ""
 	
-	# The first line consist of headings of the record
-	# so we will store it in an array and move to
-	# next line in input_file.
 	titles = [t.strip() for t in a.split('\t')]
 	for line in file:
 		d = {}
@@ -18,10 +15,13 @@ def tsv_to_json(tsv_file,json_file):
 		d["DRS URI"] = wgs_cram_drs_uri
 		arr.append(d)
 		
-	with open(json_file, 'w', encoding='utf-8') as json_file:
-		json_file.write(json.dumps(arr, indent=4))
+	return json.dumps(arr,indent=4)
+	
 
 # Driver Code
 input_filename = 'igsr_samples.tsv'
 output_filename = 'igsr_samples.json'
-tsv_to_json(input_filename,output_filename)
+json_output = tsv_to_json(input_filename)
+
+with open(output_filename, 'w', encoding='utf-8') as json_file:
+	json_file.write(json_output)
