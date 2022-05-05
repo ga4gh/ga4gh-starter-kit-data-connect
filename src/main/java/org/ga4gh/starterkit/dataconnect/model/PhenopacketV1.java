c0253557b00ba699eab3827512b7c335077059d0
yash-puligundla <yasasvini.puligundla@ga4gh.org>
2022-05-05 15:06:43 -0400

    clean up. /tables endpoint works as expected

--- src/main/java/org/ga4gh/starterkit/dataconnect/model/PhenopacketV1.java
+++ src/main/java/org/ga4gh/starterkit/dataconnect/model/PhenopacketV1.java
@@ -18,6 +18,9 @@ public class PhenopacketV1 implements HibernateEntity<String> {
     @Column(name = "id", updatable = false, nullable = false)
     private String id;
 
+    @Column(name = "json_data", updatable = false, nullable = false)
+    private String jsonData;
+
     public void loadRelations() {
 
     }
