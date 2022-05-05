449ceb3c9d4a3d9b0ddce8710295592a41ca24c3
Jeremy Adams <jeremy.adams@ga4gh.org>
2022-05-05 09:39:34 -0400

    added some phenopacket data as part of dev dataset

--- database/sqlite/one_thousand_genomes_sample/create-tables.sql
+++
@@ -1,9 +0,0 @@
-/* ################################################## */
-/* # one thousand genomes sample                                      # */
-/* ################################################## */
-
-CREATE TABLE IF NOT EXISTS one_thousand_genomes_sample
-(
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    genome_sample JSON
-);
