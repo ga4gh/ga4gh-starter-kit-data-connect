1c4dd3bfcbc153b9e669e52997ea884d104eb4e0
Jeremy Adams <jeremy.adams@ga4gh.org>
2022-05-05 09:58:01 -0400

    updated script to output drs uris for cram, crai, and bundle

--- models/one_thousand_genomes_sample/to_json.py
+++ models/one_thousand_genomes_sample/to_json.py
@@ -1,38 +1,40 @@
 import json
-import csv
 
-def tsv_to_json(tsv_file, uuid_file):
-	json_arr = []
-	uuid_arr = []
-	hostname = "localhost:5000"
-	wgs_cram_drs_uri = "drs://" + hostname + "/"
-	
-	uuid = open(uuid_file, 'r')
-	for line in uuid:
-		uuid_arr.append(line.strip())
+def tsv_to_json(tsv_file):
+    json_arr = []
+    
+    drs_uri_template_start = "drs://localhost:5000/"
+    drs_uri_template_suffix = ".1kgenomes.wgs.downsampled."
 
-	tsv = open(tsv_file, 'r')
-	a = tsv.readline()
-	n = 0
-	titles = [t.strip() for t in a.split('\t')]
-	for line in tsv:
-		d = {}
-		for t, f in zip(titles, line.split('\t')):
-			t = t.lower().replace(" ","_")
-			d[t] = f.strip()
+    tsv = open(tsv_file, 'r')
+    header = tsv.readline()
+    n = 0
+    titles = [t.strip() for t in header.split('\t')]
+    for line in tsv:
+        if n < 200:
+            sample = {}
+            for field_name, field_value in zip(titles, line.strip().split('\t')):
+                field_name = field_name.lower().replace(" ","_")
+                sample[field_name] = field_value
+            
+            drs_uri_template = drs_uri_template_start + sample["sample_name"] + drs_uri_template_suffix
+            sample["cram_drs_uri"] = drs_uri_template + "cram"
+            sample["crai_drs_uri"] = drs_uri_template + "crai"
+            sample["bundle_drs_uri"] = drs_uri_template + "bundle"
 
-		d["wgs_cram_drs_uri"] = wgs_cram_drs_uri + uuid_arr[n]
-		json_arr.append(d)
-		n += 1
-	
-	return json.dumps(json_arr,indent=4)
-	
+            json_arr.append(sample)
+        n += 1
 
-# Driver Code
-input_filename = "igsr_samples.tsv"
-uuid_filename = "uuid.csv"
-output_filename = "igsr_samples.json"
-json_output = tsv_to_json(input_filename, uuid_filename)
+    return json.dumps(json_arr,indent=4)
 
-with open(output_filename, 'w', encoding='utf-8') as json_file:
-	json_file.write(json_output)
+def main():
+    # Driver Code
+    input_filename = "igsr_samples.tsv"
+    output_filename = "igsr_samples.json"
+    json_output = tsv_to_json(input_filename)
+
+    with open(output_filename, 'w', encoding='utf-8') as json_file:
+        json_file.write(json_output)
+
+if __name__ == "__main__":
+    main()
