54db6901db2e4337040a85d654a80674fcba4054
yash-puligundla <yasasvini.puligundla@ga4gh.org>
2022-05-09 16:08:02 -0400

    update /tables tests. need to fix phenopacketv1 data tests

--- src/test/java/org/ga4gh/starterkit/dataconnect/controller/TablesTest.java
+++ src/test/java/org/ga4gh/starterkit/dataconnect/controller/TablesTest.java
@@ -2,6 +2,9 @@ package org.ga4gh.starterkit.dataconnect.controller;
 
 import org.ga4gh.starterkit.dataconnect.app.DataConnectServer;
 import org.ga4gh.starterkit.dataconnect.app.DataConnectServerSpringConfig;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.skyscreamer.jsonassert.JSONParser;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
@@ -9,6 +12,7 @@ import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.ResultMatcher;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
@@ -17,6 +21,8 @@ import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+import testutils.ResourceLoader;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @ContextConfiguration(classes = {
@@ -39,49 +45,82 @@ public class TablesTest extends AbstractTestNGSpringContextTests {
 
     @DataProvider(name = "getTableCases")
     public Object[][] getTableCases() {
-        return new Object[][] {
-            { "exampleData" },
-            { "variant" },
-            { "phenopacket" }
+        return new Object[][]{
+                {
+                        "one_thousand_genomes_sample",
+                        status().isOk()
+                },
+                {
+                        "phenopacket_v1",
+                        status().isOk()
+                }
         };
     }
 
-    // TODO Create proper tests when the list tables endpoint is implemented.
     @Test
     public void testListTables() throws Exception {
+        // From the list of all tables, we only validate the details of
+        // phenopacket_v1 and one_thousand_genomes_sample tables
+
         MvcResult result = mockMvc.perform(MockMvcRequestBuilders.get("/tables"))
             .andExpect(MockMvcResultMatchers.status().isOk())
             .andReturn();
         String responseBody = result.getResponse().getContentAsString();
-        String expResponseBody = "You hit the /tables endpoint. This method is a stub.";
+
+        JSONObject dataObj = (JSONObject) JSONParser.parseJSON(responseBody);
+
+        //Get the required object from the above created object
+        JSONArray tablesList = (JSONArray) dataObj.get("tables");
+
+        boolean phenopacketV1Present = false;
+        boolean oneThousandGenomesSamplePresent = false;
+
+        for (int i = 0; i < tablesList.length(); i++) {
+
+            JSONObject tableDetails = (JSONObject) tablesList.get(i);
+
+            // Assert the table details for phenopacket_v1 and one_thousand_genomes_sample tables
+            if (tableDetails.get("name").equals("phenopacket_v1")) {
+                phenopacketV1Present = true;
+                Assert.assertEquals(tableDetails.get("description"),"Table / directory containing JSON files for phenopackets");
+                JSONObject dataModel = (JSONObject) tableDetails.get("data_model");
+                Assert.assertEquals(dataModel.get("$ref"),"table/phenopacket_v1/info");
+            } else if(tableDetails.get("name").equals("one_thousand_genomes_sample")){
+                oneThousandGenomesSamplePresent = true;
+                Assert.assertEquals(tableDetails.get("description"),"Table / directory containing JSON files for 1000 genome samples from https://www.internationalgenome.org");
+                JSONObject dataModel = (JSONObject) tableDetails.get("data_model");
+                Assert.assertEquals(dataModel.get("$ref"),"table/one_thousand_genomes_sample/info");
+            }
+            if (phenopacketV1Present && oneThousandGenomesSamplePresent){
+                break;
+            }
+        }
+
+        // Assert that the default tables, "phenopacket_v1" and "one_thousand_genomes_sample" are present in the list of tables
+        Assert.assertEquals(phenopacketV1Present, true);
+        Assert.assertEquals(oneThousandGenomesSamplePresent, true);
+
+    }
+
+    @Test(dataProvider = "getTableCases")
+    public void testGetTableInfo(String tableName, ResultMatcher expStatus) throws Exception {
+        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.get("/table/" + tableName + "/info"))
+            .andExpect(expStatus)
+            .andReturn();
+        String responseBody = result.getResponse().getContentAsString();
+        String expResponseBody = ResourceLoader.load(String.format("/responses/tables/GetTableInfo/table_info_%s.json",tableName));
         Assert.assertEquals(responseBody, expResponseBody);
     }
 
-    // TODO Create proper tests when the get table info endpoint is implemented.
+    // TODO: fix this!
     @Test(dataProvider = "getTableCases")
-    public void testGetTableInfo(String tableName) throws Exception {
-        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.get("/tables/" + tableName + "/info"))
-            .andExpect(MockMvcResultMatchers.status().isOk())
+    public void testGetTableData(String tableName, ResultMatcher expStatus)
+            throws Exception {
+        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.get("/table/" + tableName + "/data"))
+            .andExpect(expStatus)
             .andReturn();
         String responseBody = result.getResponse().getContentAsString();
-        String expResponseBody =
-            "You hit the /tables/{table_name}/info endpoint."
-            + " This method is a stub."
-            + " You requested the table: '" + tableName + "'";
-        Assert.assertEquals(responseBody, expResponseBody);
-    }
-
-    // TODO Create proper tests when the get table endpoint is implemented.
-    @Test(dataProvider = "getTableCases")
-    public void testGetTableData(String tableName) throws Exception {
-        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.get("/tables/" + tableName + "/data"))
-            .andExpect(MockMvcResultMatchers.status().isOk())
-            .andReturn();
-        String responseBody = result.getResponse().getContentAsString();
-        String expResponseBody =
-            "You hit the /tables/{table_name}/data endpoint."
-            + " This method is a stub."
-            + " You requested the table: '" + tableName + "'";
+        String expResponseBody = ResourceLoader.load(String.format("/responses/tables/GetTableData/table_data_%s.json",tableName));
         Assert.assertEquals(responseBody, expResponseBody);
     }
 }
